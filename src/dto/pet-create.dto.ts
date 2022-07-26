94db771f97ab6f54ad8005d9c8cbb3866123f57a
Amadeusz Blanik <amadeusz@blanik.me>
2022-07-26 18:04:58 +0200

    Added birthdate validators

--- src/dto/pet-create.dto.ts
+++ src/dto/pet-create.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsDate, IsNotEmpty, MaxDate, MinDate } from 'class-validator';
+import { Transform } from 'class-transformer';
 import { PetKind } from '../types/pet-kind.types';
 
 export class PetCreateDto {
@@ -29,8 +30,12 @@ export class PetCreateDto {
   microchip: string;
 
   @ApiProperty({
-    description: 'Birth date',
+    description: 'Birth date. Must be between 01-01-1970 and current date',
     example: new Date('12-02-2022'),
   })
+  @Transform(({ value }: { value: Date | string }) => new Date(value))
+  @IsDate()
+  @MaxDate(new Date())
+  @MinDate(new Date('01-01-1970'))
   birthDate: Date;
 }
--- src/dto/pet-update.dto.ts
+++ src/dto/pet-update.dto.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, MaxDate, MinDate } from 'class-validator';
+import { Transform } from 'class-transformer';
 import { PetKind } from '../types/pet-kind.types';
 
 export class PetUpdateDto {
@@ -27,8 +29,12 @@ export class PetUpdateDto {
   microchip?: string;
 
   @ApiProperty({
-    description: 'Birth date',
+    description: 'Birth date. Must be between 01-01-1970 and current date',
     example: new Date('12-02-2022'),
   })
+  @Transform(({ value }: { value: Date | string }) => new Date(value))
+  @IsDate()
+  @MaxDate(new Date())
+  @MinDate(new Date('01-01-1970'))
   birthDate?: Date;
 }
